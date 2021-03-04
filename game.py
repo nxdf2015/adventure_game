19ffb24f2e38198541eaf11983fa71e354171618
xavier niquet <niquet_xavier2002@yahoo.fr>
2021-03-04 17:51:52 +0100

    stage 2

--- game.py
+++ game.py
@@ -21,17 +21,54 @@ def get_selection(*values):
 
         print("Unknown input! Please enter a valid one.")
 
+def user():
+    choice = input("Enter a user name to save your progress or type '/b' to go back")
+
+    if choice == "/b":
+        print("Going back to menu...")
+        return False
+    else:
+        user =  { "username" : choice , "characters": {}, "bag" : {}}
+
+        print("Create your character:")
+        characters = ["name","species","gender"]
+
+        for i,character in enumerate(characters,1):
+            user["characters"][character] = input(f"{i}-{character}")
+
+        print("Pack your bag for the journey:")
+        user["bag"]["snack"] = input("Favorite snack")
+        user["bag"]["weapon"] = input("A weapon for the journey")
+        user["bag"]["tool"] = input("A traversal tool")
+
+        print("""Choose your difficulty:
+1- Easy
+2- Medium
+3- Hard
+""")
+        difficulties= ["easy","medium","hard"]
+        user["difficulty"] = get_selection(* difficulties)
+        print("Good luck on your journey!")
+        print("Your character: ", ", ".join(user["characters"].values()))
+        print("Your inventory: ", ", ".join(user["bag"].values()))
+
+
+        print("Difficulty :", difficulties[user["difficulty"] - 1])
+        return True
+
 
 def game():
-    print(menu)
-    choice = get_selection("start", "load", "quit")
-    if choice == 1:
-        print("Starting a new game...")
-    elif choice == 2:
-        print("No save data found!")
-
+    stop_game = False
+    while not stop_game:
+        print(menu)
+        choice = get_selection("start", "load", "quit")
+        if choice == 1:
+            print("Starting a new game...")
+            stop_game = user()
+        elif choice == 2:
+            print("No save data found!")
+        else:
+            break
     print("Goodbye!")
-
-    print()
 
 game()
